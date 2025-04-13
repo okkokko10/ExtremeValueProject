b4f3e646c2934877f2fd348202c392f46073f1a3
Kalle <kalle.kytola@aalto.fi>
2025-04-14 00:07:47 +0300

    Write some blueprint.

--- ExtremeValueProject.lean
+++ ExtremeValueProject.lean
@@ -1,3 +1,8 @@
 -- This module serves as the root of the `ExtremeValueProject` library.
 -- Import modules here that should be built as part of the library.
-import «ExtremeValueProject».Basic
+import «ExtremeValueProject».AffineTransformation
+import «ExtremeValueProject».CumulativeDistributionFunction
+import «ExtremeValueProject».DegenerateCDF
+import «ExtremeValueProject».ExtremeValueDistribution
+import «ExtremeValueProject».PseudoInverses
+import «ExtremeValueProject».TransformedCDF
